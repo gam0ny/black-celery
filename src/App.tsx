d55731753547a6cafcd7953a1296dcc7e3a76379
Alina Siomukha <tonks.alina@gmail.com>
2024-04-22 13:41:24 +0200

    Implement some base components to render posts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header'
+import Posts from './components/posts'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
